144bdc288866ee7c1e7b66549eeffe191e596bf5
Filipe Fernandes <ocefpaf@gmail.com>
2018-12-13 08:50:10 -0200

    try --no-as-needed

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,8 @@ export UWSGI_PROFILE="${RECIPE_DIR}/uwsgi_config.ini"
 export UWSGI_INCLUDES="${PREFIX}/include,${PREFIX}/include/openssl"
 export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
 
+if [[ $(uname) == Linux ]]; then
+  export LDFLAGS="${LDFLAGS} -Wl,--no-as-needed"
+fi
+
 UWSGI_EMBED_PLUGINS=stats_pusher_statsd ${PYTHON} -m pip install . --no-deps -vv
